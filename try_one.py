b5b26e451b5363a9978c544574490969d4373e04
YushRajcoomar <yush2398@live.com>
2024-03-06 13:53:40 +0000

    generate predictions for onnx-oyo-t5-model

--- try_one.py
+++ try_one.py
@@ -18,7 +18,7 @@ def _extract_yoruba_sentences(file_path:str):
 
     return yoruba_sentences
 
-def diacritize(texts=[]):
+def diacritize(file_no, texts=[]):
     # Load encoder model
     encoder_session = onnxruntime.InferenceSession('quantized_oyto_t5_small_onnx/encoder_model_quantized.onnx')
 
@@ -42,15 +42,15 @@ def diacritize(texts=[]):
     
     inputs = tokenizer(texts, return_tensors="pt", padding=True, truncation=True)
     
-    gen_tokens = model.generate(**inputs, use_cache=True, max_new_tokens=100000)
+    gen_tokens = model.generate(**inputs, use_cache=True)
     outputs = tokenizer.batch_decode(gen_tokens, skip_special_tokens=True)
-    
+    # name_of_file = f"diacritse_output_{file_num}.txt"
     # Write outputs to file
-    with open("/mnt/disk/makindele/lafand-mt/diacritse_output.txt", "w", encoding="utf-8") as output_file:
+    with open(f"/mnt/disk/makindele/lafand-mt/sample_out.txt", "w", encoding="utf-8") as output_file:
         for text in outputs:
             output_file.write(text + "\n")
     
-    print("Outputs written to file: diacritse_output.txt")
+    # print(f"Outputs written to file: {name_of_file}")
     return outputs
 
    
@@ -65,6 +65,8 @@ def diacritize(texts=[]):
 # text = "Nje omo naa ni owo?" # mo lo si ile
 # output = diacritize(text=text)
 # print(f'Result of translation is = {output}')
+# missing files: 40, 60, 160, 170,270, 280, 300, 400, 420, 460
 
 absolute_path = Path('/mnt/disk/makindele/data_prep_eng/data_prep_eng/output_data/test_with_no_diacritics.txt').resolve()
-diacritize(texts=_extract_yoruba_sentences(file_path=absolute_path)[:])
+# file_num = 640
+diacritize(texts=_extract_yoruba_sentences(file_path=absolute_path)[:], file_no=0 )
